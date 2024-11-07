fe1303c72ef7ba185a9baac16dddc064e2e1bc83
Garry Trinder <garry@trinder365.co.uk>
2024-11-07 15:47:45 +0000

    Add 446 BEGIN and END solutions

---
+++ LAB-446/LAB-446-END/Custom.Engine.Agent/Actions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Teams.AI.AI.Action;
+using Microsoft.Teams.AI.AI.Planners;
+using Microsoft.Teams.AI.AI;
+using AdaptiveCards;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json.Linq;
+
+namespace Custom.Engine.Agent;
+
+internal class Actions
+{
+    [Action(AIConstants.SayCommandActionName, isDefault: false)]
+    public static async Task<string> SayCommandAsync([ActionTurnContext] ITurnContext turnContext, [ActionParameters] PredictedSayCommand command, CancellationToken cancellationToken = default)
+    {
+        IMessageActivity activity;
+        if (command?.Response?.Context?.Citations?.Count > 0)
+        {
+            AdaptiveCard card = ResponseCardCreator.CreateResponseCard(command.Response);
+            Attachment attachment = new()
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = card
+            };
+            activity = MessageFactory.Attachment(attachment);
+
+            activity.Entities =
+            [
+                new Entity
+                {
+                    Type = "https://schema.org/Message",
+                    Properties = new()
+                    {
+                        { "@type", "Message" },
+                        { "@context", "https://schema.org" },
+                        { "@id", string.Empty },
+                        { "additionalType", JArray.FromObject(new string[] { "AIGeneratedContent" } ) },
+                        { "usageInfo", JObject.FromObject(
+                            new JObject(){
+                                { "@type", "CreativeWork" },
+                                { "name", "Confidential" },
+                                { "description", "Sensitive information, do not share outside of your organization." },
+                            })
+                        }
+                    }
+                }
+            ];
+        }
+        else
+        {
+            activity = MessageFactory.Text(command.Response.GetContent<string>());
+        }
+
+        activity.ChannelData = new
+        {
+            feedbackLoopEnabled = true
+        };
+
+        await turnContext.SendActivityAsync(activity, cancellationToken);
+
+        return string.Empty;
+    }
+}
